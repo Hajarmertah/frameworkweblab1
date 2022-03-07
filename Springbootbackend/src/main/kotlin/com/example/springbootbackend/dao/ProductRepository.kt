4efac04db001097d553dac76f2410a41a9d17131
Mertah Hajar <74379716+Hajarmertah@users.noreply.github.com>
2022-03-07 22:53:53 +0100

    Update ProductRepository.kt

--- Springbootbackend/src/main/kotlin/com/example/springbootbackend/dao/ProductRepository.kt
+++ Springbootbackend/src/main/kotlin/com/example/springbootbackend/dao/ProductRepository.kt
@@ -1,6 +1,6 @@
 package com.example.springbootbackend.dao
 
-import com.example.springbootkt.data.Product
+import com.example.springbootbackend.data.Product
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.rest.core.annotation.RepositoryRestResource
 import org.springframework.web.bind.annotation.CrossOrigin
